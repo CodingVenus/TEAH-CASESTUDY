c12adecb190b4dda02bf7bde4920eea7c262534d
Venus Teah <venus.teah@gmail.com>
2023-02-09 18:27:10 -0500

    Create skeleton of project

---
+++ Venus_Teah_PokeTeam_CaseStudy/src/main/java/com/example/venus_teah_poketeam_casestudy/VenusTeahPokeTeamCaseStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.venus_teah_poketeam_casestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenusTeahPokeTeamCaseStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VenusTeahPokeTeamCaseStudyApplication.class, args);
+    }
+
+}
---
+++ Venus_Teah_PokeTeam_CaseStudy/src/test/java/com/example/venus_teah_poketeam_casestudy/VenusTeahPokeTeamCaseStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.venus_teah_poketeam_casestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenusTeahPokeTeamCaseStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
