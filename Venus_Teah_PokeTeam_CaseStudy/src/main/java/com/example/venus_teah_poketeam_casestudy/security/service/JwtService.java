e50860353f8027c2619ce58fc1ab3c208e51351f
Venus Teah <venus.teah@gmail.com>
2023-02-15 13:13:27 -0500

    Comment out security

--- Venus_Teah_PokeTeam_CaseStudy/src/main/java/com/example/venus_teah_poketeam_casestudy/security/service/JwtService.java
+++ Venus_Teah_PokeTeam_CaseStudy/src/main/java/com/example/venus_teah_poketeam_casestudy/security/service/JwtService.java
@@ -1,20 +1,55 @@
-package com.example.venus_teah_poketeam_casestudy.security.service;
-
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import org.springframework.stereotype.Service;
-
-@Service
-public class JwtService {
-    private static final String SECRET_KEY = "2948404D635166546A576E5A7234753777217A25432A462D4A614E645267556B";
-
-    private Claims extractAllClaims(String jwtObj) {
-
-        return Jwts
-                .parserBuilder()
-                .setSigningKey(getSignInKey())
-                .build()
-                .parseClaimsJws(jwtObj)
-                .getBody();
-    }
-}
+//package com.example.venus_teah_poketeam_casestudy.security.service;
+//
+//import io.jsonwebtoken.Claims;
+//import io.jsonwebtoken.Jwts;
+//import io.jsonwebtoken.io.Decoders;
+//import io.jsonwebtoken.security.Keys;
+//import org.springframework.stereotype.Service;
+//
+//import java.security.Key;
+//import java.util.Base64;
+//import java.util.function.Function;
+//
+//@Service
+//public class JwtService {
+//    private static final String SECRET_KEY = "2948404D635166546A576E5A7234753777217A25432A462D4A614E645267556B";
+//
+//    //SIGNATURE
+//    private Key getSignInKey() {
+////        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+//        byte[] decodedKey = Base64.getDecoder().decode(SECRET_KEY);
+//        return Keys.hmacShaKeyFor(decodedKey);
+//    }
+//
+//    //EXTRACT CLAIMS OF TOKEN
+//    private Claims extractAllClaims(String jwt) {
+//
+//        return Jwts
+//                .parserBuilder()
+//                .setSigningKey(getSignInKey())
+//                .build()
+//                .parseClaimsJws(jwtObj)
+//                .getBody();
+//    }
+//
+//    public <T> T extractClaim(String jwt, Function<Claims, T> Resolver) {
+//        final Claims claims = extractAllClaims(jwt);
+//        return Resolver.apply(claims);
+//    }
+//
+//
+//    //
+//
+//
+//
+//
+//
+//
+//    //Subject Token = Username/Email
+//    public String extractSubjectToken(String jwtObj) {
+//        return
+//    }
+//
+//
+//
+//}
